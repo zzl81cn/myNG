d6b4fa99e8efb9fdbfacfcf7f173370f96e75ef9
zzl81cn <zzl81cn@163.com>
2016-09-25 17:28:58 +0800

    1.Modify angular-ui-router example.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /**
- * 
+ *
  * @authors Your Name (you@example.org)
  * @date    2016-09-25 15:00:35
  * @version $Id$
  */
 
 var alertModule = angular.module('alertApp', [
-		'ui.router'
-	]);
+	'ui.router'
+]);
 alertModule.config(function($stateProvider, $urlRouterProvider){
 
 	// 如果没有匹配的URL，就定向到此URL地址
@@ -35,5 +35,26 @@ alertModule.config(function($stateProvider, $urlRouterProvider){
 			controller: function($scope){
 				$scope.things = ["A", "Set", "Of", "Things"];
 			}
-		});
+		})
+		.state('index', {
+			url: '',
+			views: {
+				"viewA": {template: "index.viewA"},
+				"viewB": {template: "index.viewB"}
+			}
+		})
+		.state('route1', {
+			url: '/route1',
+			views: {
+				"viewA": {template: "route1.viewA"},
+				"viewB": {template: "route1.viewB"}
+			}
+		})
+		.state('route2', {
+			url: '/route2',
+			views: {
+				"viewA": {template: "route2.viewA"},
+				"viewB": {template: "route2.viewB"}
+			}
+		})
 });

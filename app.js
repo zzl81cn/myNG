bded173ef6b732177a31511abe75cd0cc12b8c71
zzl81cn <zzl81cn@163.com>
2016-09-25 16:02:57 +0800

    1.Modify angular-ui-router example.

--- app.js
+++ app.js
@@ -8,4 +8,32 @@
 var alertModule = angular.module('alertApp', [
 		'ui.router'
 	]);
-// alertModule.config();
+alertModule.config(function($stateProvider, $urlRouterProvider){
+
+	// 如果没有匹配的URL，就定向到此URL地址
+	$urlRouterProvider.otherwise('/state1');
+
+	$stateProvider
+		.state('state1', {
+			url: '/state1',
+			templateUrl: 'partials/state1.html'
+		})
+		.state('state1.list', {
+			url: '/list',
+			templateUrl: 'partials/state1.list.html',
+			controller: function($scope){
+				$scope.items = ["A", "List", "Of", "Items"];
+			}
+		})
+		.state('state2', {
+			url: '/state2',
+			templateUrl: 'partials/state2.html'
+		})
+		.state('state2.list', {
+			url: '/list',
+			templateUrl: 'partials/state2.list.html',
+			controller: function($scope){
+				$scope.things = ["A", "Set", "Of", "Things"];
+			}
+		});
+});
